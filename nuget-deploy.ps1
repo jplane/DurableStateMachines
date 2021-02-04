c837f255f2d6a456a70eb468eddb962654a9c177
Josh Lane <jolan@microsoft.com>
2021-02-04 13:37:42 -0500

    updated nuget bits

---
+++ nuget-deploy.ps1
@@ -0,0 +1,8 @@
+
+$key = ''
+
+dotnet nuget push .\Engine\bin\Release\DurableStateMachines.Core.0.1.0.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+
+dotnet nuget push .\FunctionHost\bin\Release\DurableStateMachines.Functions.0.1.0.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+
+dotnet nuget push .\FunctionClient\bin\Release\DurableStateMachines.Client.0.1.0.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
