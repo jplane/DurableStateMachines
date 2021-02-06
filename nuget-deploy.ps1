4e54a3e62c2bdbfc2be1ea1cc56fe73f1457d25a
Josh Lane <jolan@microsoft.com>
2021-02-06 14:30:15 -0500

    0.1.3 -> 0.1.4

--- nuget-deploy.ps1
+++ nuget-deploy.ps1
@@ -1,8 +1,8 @@
 
 $key = ''
 
-dotnet nuget push .\Engine\bin\Release\DurableStateMachines.Core.0.1.3.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\Engine\bin\Release\DurableStateMachines.Core.0.1.4.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
 
-dotnet nuget push .\FunctionHost\bin\Release\DurableStateMachines.Functions.0.1.3.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\FunctionHost\bin\Release\DurableStateMachines.Functions.0.1.4.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
 
-dotnet nuget push .\FunctionClient\bin\Release\DurableStateMachines.Client.0.1.3.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\FunctionClient\bin\Release\DurableStateMachines.Client.0.1.4.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
